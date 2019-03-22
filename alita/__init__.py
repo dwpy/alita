e8e6b254605fdce3404d5726a9fa15fb37168ba3
dongwei <dongwei@gstianfu.com>
2019-03-22 16:38:50 +0800

    change alita version

--- alita/__init__.py
+++ alita/__init__.py
@@ -4,4 +4,4 @@ from alita.blueprints import Blueprint
 from alita.response import HtmlResponse, TextResponse, JsonResponse
 from alita.templating import render_template, render_template_string
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
